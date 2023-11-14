ef30eaee1647221950b1b0b25c07ec2e1511409d
Stanislav T <0305stas@inbox.lv>
2023-11-14 11:27:28 +0200

    Added TypeScript

--- src/electron.js
+++ src/electron.js
@@ -1,15 +1,15 @@
 // src/electron.js
-const { app, BrowserWindow } = require('electron');
-function createWindow () {
-  // Create the browser window.
-  let win = new BrowserWindow({
-    width: 800,
-    height: 600,
-    webPreferences: {
-      nodeIntegration: true
-    }
-  });
-  // and load the index.html of the app.
-  win.loadFile('index.html');
+var _a = require('electron'), app = _a.app, BrowserWindow = _a.BrowserWindow;
+function createWindow() {
+    // Create the browser window.
+    var win = new BrowserWindow({
+        width: 800,
+        height: 600,
+        webPreferences: {
+            nodeIntegration: true
+        }
+    });
+    // and load the index.html of the app.
+    win.loadFile('index.html');
 }
 app.on('ready', createWindow);
